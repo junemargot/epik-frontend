38ab610cedc484d94d7752864f758b00d985385c
EJH <margotinjune@gmail.com>
2025-10-18 22:57:01 +0900

    feat: 북마크, 알림 기능 추가

--- components/event/EventHeader.vue
+++ components/event/EventHeader.vue
@@ -10,22 +10,64 @@
         </div>
         <div class="event__icon">
           <a href="#" @click.prevent="$emit('bookmark-click')">
-            <i class="bx bx-bell"></i>
+            <i class="bx bx-bookmark"></i>
           </a>
           <span>북마크</span>
         </div>
       </div> -->
+      <div class="actions">
+        <button @click="$emit('notification-click')" class="btn-icon">
+          <i class="bx bx-bell"></i>
+        </button>
+        <button 
+          @click="$emit('bookmark-click')" 
+          class="btn-icon"
+          :class="{ 'bookmarked': isBookmarked }"
+        >
+          <i :class="isBookmarked ? 'bx bxs-bookmark' : 'bx bx-bookmark'"></i>
+        </button>
+      </div>
     </div>
 </template>
 
 <script setup lang="ts">
   const props = defineProps<{
-    title: string
+    title: string,
+    isBookmarked: {
+      type: Boolean,
+      default: false
+    }
   }>()
 
+defineEmits(['notification-click', 'bookmark-click']);
 </script>
 
 <style>
 @import url('/public/css/components/event.css');
+.actions {
+  display: flex;
+  gap: 14px;
+}
+
+.btn-icon {
+  background: none;
+  border: none;
+  font-size: 28px;
+  cursor: pointer;
+  color: #333;
+  transition: color .3s;
+}
+
+.btn-icon:hover {
+  color: #ff6b6b;
+}
+
+.btn-icon.bookmarked {
+  color: #ff6b6b;
+}
+
+.btn-icon i {
+  display: block;
+}
 </style>
 
